ca9b276c2c9e6b42660fc59b6001f3e31b7af2ba
StevenXHY <605195407yang@gmail.com>
2022-04-12 09:55:24 +0800

    commit

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -15,19 +15,38 @@ public class Demo {
         InputStream iems = new FileInputStream("");
         Workbook wbcp = Workbook.getWorkbook(cp);
         Workbook wbiems = Workbook.getWorkbook(iems);
-        Sheet lotIdSheet = wbiems.getSheet(1).getColumn();
-        Sheet  waferListSheet= wbiems.getSheet("WAFERLIST");
-        Sheet cplotIdSheet = wbcp.getSheet("LOT_ID");
-        Sheet cpWaferIDSheet = wbcp.getSheet("WAFER_ID");
-        Sheet cpYieldSheet = wbcp.getSheet("CP1_YIELD");
+        Cell[] iemslotId = wbiems.getSheet(1).getColumn(1);
+        Cell[]  iemswaferList= wbiems.getSheet("WAFERLIST").getColumn(11);
+        Cell[] cplotId = wbcp.getSheet("LOT_ID").getColumn(0);
+        Cell[] cpWaferID = wbcp.getSheet("WAFER_ID").getColumn(1);
+        Cell[] cpYield = wbcp.getSheet("CP1_YIELD").getColumn(5);
     }
 
-    public String getIemsResultByLotID(Sheet lotIdSheet,Sheet waferListSheet){
+    public String getIemsResultByLotID(Cell[] iemslotId,Cell[]  iemswaferList,Cell[] cplotId,Cell[] cpWaferID,Cell[] cpYield){
         //获取行数
-        int rows = lotIdSheet.getRows();
-        for(int i=0;i<rows;i++){
-            Cell[] row = lotIdSheet.getRow(i);
-
+        int iemsrows = iemslotId.length;
+        for(int i=0;i<iemsrows;i++){
+            String lotID = iemslotId[i].getContents();
+            String waferList =iemswaferList[i].getContents();
+            String[] waferArray = waferList.split(" ");
+            int waferArrayRows = waferArray.length;
+            int cprows = cplotId.length;
+            double sum = 0;
+            int count = 0;
+            for(int j=0;j<cprows;j++){
+                String WaferID = cpWaferID[j].getContents();
+                double Yield = Double.parseDouble(cpYield[j].getContents());
+                if (lotID.equals(cplotId[j])){
+                    for(int x=0;x<waferArrayRows;x++){
+                        if(waferArray[x]==WaferID){
+                            sum+=Yield;
+                            count+=1;
+                        }
+                    }
+               }
+            }
+            double result = sum / count;
+            System.out.println("LotID:"+lotID+"-------result:"+result);
         }
         return "";
     }
